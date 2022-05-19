36031581bcb654fdfc12f0bf23fa895c38858348
Lucas Molin <lucas.molin@estudante.ufjf.br>
2022-05-18 22:18:10 -0300

    Altera componente e texto

--- app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
@@ -2,13 +2,20 @@ package br.ufjf.dcc196.igorknop.meustarter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.usage.UsageEvents;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String msg = "Olá mundooooooooo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textViewMsg = findViewById(R.id.textViewMsg);
+        textViewMsg.setText(msg);
+        textViewMsg.setTextSize(36);
     }
 }
