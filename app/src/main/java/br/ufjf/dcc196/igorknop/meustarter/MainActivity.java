eb15c6bdff86d009215d897c11a5838b6fdbddc2
Lucas Molin <lucas.molin@estudante.ufjf.br>
2022-05-18 22:32:35 -0300

    Altera ids e campo de entrada

--- app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.usage.UsageEvents;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String msg = "Olá mundooooooooo";
+    private String msg = "Olá mundo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicaBotaoMsg(View origem) {
+        msg += "o";
         TextView textViewMsg = findViewById(R.id.textViewMsg);
         textViewMsg.setText(msg);
         textViewMsg.setTextSize(36);
