bc39e22ae2ae7dd1fa206e1edad3d4f1f9ec7ebe
Lucas Molin <lucas.molin@estudante.ufjf.br>
2022-05-18 22:49:33 -0300

    Extrai texto de componentes e mostra texto

--- app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/igorknop/meustarter/MainActivity.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.usage.UsageEvents;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String msg = "Olá mundo";
+    private EditText editTextStarter;
+    private EditText editTextName;
+    private TextView textViewMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextStarter = findViewById(R.id.editTextStarter);
+        editTextName = findViewById(R.id.editTextTextPersonName);
+        textViewMsg = findViewById(R.id.textViewMsg);
     }
 
     public void clicaBotaoMsg(View origem) {
-        msg += "o";
-        TextView textViewMsg = findViewById(R.id.textViewMsg);
+        String nameTxt = editTextName.getText().toString();
+        String starterTxt = editTextStarter.getText().toString();
+        String msg = String.format("Olá seu nome é %s e seu starter é %s", nameTxt, starterTxt);
         textViewMsg.setText(msg);
         textViewMsg.setTextSize(36);
+
     }
 }
